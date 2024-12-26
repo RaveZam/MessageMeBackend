39cffce93c2620beb5a84ca8d6b528f7d739b382
Duke <114975455+RaveZam@users.noreply.github.com>
2024-12-27 00:25:10 +0800

    JWT tokens for session applied with the login, and bcrypt for passwords while registering

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,12 +1,50 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const AddUser = async (req, res) => {
+  // try {
+  //   const user = await User.create(req.body);
+  //   res.status(200).json(user);
+  // } catch (error) {
+  //   res.status(500).json({ message: error.message });
+  // }
+  const { username, email, password } = req.body;
   try {
-    const user = await User.create(req.body);
-    res.status(200).json(user);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User Registered Successfully" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Error Registering User", error });
   }
 };
 
-module.exports = { AddUser };
+const JWT_SECRET = "thisisasecret";
+
+const Login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid Password" });
+    }
+
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ status: 200, message: "Login Successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error ", error });
+  }
+};
+
+module.exports = { AddUser, Login };
--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { AddUser } = require("../Controllers/user.controller");
+const { AddUser, Login } = require("../Controllers/user.controller");
 
 router.post("/AddUser", AddUser);
+router.post("/Login", Login);
 
 module.exports = router;
