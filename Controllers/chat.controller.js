0a1c1279dc2ac56081dfe00cb4cabad2fef73be2
Duke <114975455+RaveZam@users.noreply.github.com>
2024-12-29 21:59:48 +0800

    fetching Chatrooms via by participants of the current user

--- Controllers/chat.controller.js
+++ Controllers/chat.controller.js
@@ -1,7 +1,26 @@
 const User = require("../models/user.model");
 const Chat = require("../models/chat.model");
 
-const getChatRooms = async (req, res) => {};
+const getChatRooms = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const chatRooms = await Chat.find({
+      participants: id,
+    });
+
+    if (!chatRooms.length) {
+      res.status(404).json({ message: "No Rooms Found!" });
+    }
+    console.log(chatRooms);
+    res.status(200).json({
+      chatRooms,
+      message: "Chatrooms found successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error Loading Chatrooms", error });
+  }
+};
 
 const CreateRoom = async (req, res) => {
   const { participants, chatname } = req.body;
@@ -17,4 +36,4 @@ const CreateRoom = async (req, res) => {
 
 const sendMessage = async (req, res) => {};
 
-module.exports = { CreateRoom, sendMessage };
+module.exports = { CreateRoom, sendMessage, getChatRooms };
--- Routes/chat.route.js
+++ Routes/chat.route.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { CreateRoom, sendMessage } = require("../Controllers/chat.controller");
+const {
+  CreateRoom,
+  sendMessage,
+  getChatRooms,
+} = require("../Controllers/chat.controller");
 
 router.post("/CreatRoom", CreateRoom);
 router.post("/sendMessage", sendMessage);
+router.post("/getChatRooms", getChatRooms);
 
 module.exports = router;
